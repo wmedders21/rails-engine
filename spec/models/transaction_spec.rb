e3832c8406259d1ae3599118b46ee8ee5344d8e6
Will Medders <wmedders@gmail.com>
2022-05-16 23:45:53 -0400

    Model Test: Transaction relationships and validations

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   it 'relationships' do
-
+    should { belong_to(:invoices) }
   end
 
   it 'validations' do
-
+    should { validate_presence_of(:invoice_id) }
+    should { validate_presence_of(:credit_card_number) }
+    should { validate_presence_of(:credit_card_expiration_date) }
+    should { validate_presence_of(:result) }
   end
 end
