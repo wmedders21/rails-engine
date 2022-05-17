de9ea1c3b3a312e17b0f138b0596814b0eaa0c1a
Will Medders <wmedders@gmail.com>
2022-05-17 00:15:32 -0400

    Feature: Model relationships and validations

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,9 @@
 class InvoiceItem < ApplicationRecord
+  belongs_to :invoice
+  belongs_to :item
+  has_one :merchant, through: :invoice
+  has_one :customer, through: :invoice
 
+  validates_presence_of :item_id
+  validates_presence_of :invoice_id
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to(:invoice) }
     it { should belong_to(:item) }
-    it { should have_one(:merchant).through(:invoices) }
-    it { should have_one(:customer).through(:invoices) }
+    it { should have_one(:merchant).through(:invoice) }
+    it { should have_one(:customer).through(:invoice) }
   end
 
   describe 'validations' do
