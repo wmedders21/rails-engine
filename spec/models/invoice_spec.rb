73864de4189360733970a586daf0e2f8f6009ea2
Will Medders <wmedders@gmail.com>
2022-05-16 23:35:13 -0400

    Model Test: Invoice relationships and validations

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   it 'relationships' do
-
+    should { belong_to(:merchant) }
+    should { belong_to(:customer) }
+    should { have_many(:transactions) }
+    should { have_many(:invoice_items) }
+    should { have_many(:items).through(:invoice_items) }
   end
 
   it 'validations' do
-
+    should { validate_presence_of(:customer_id) }
+    should { validate_presence_of(:merchant_id) }
+    should { validate_presence_of(:status) }
   end
 end
