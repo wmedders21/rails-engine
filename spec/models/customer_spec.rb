20d4c15558fcacaa3ca7357e497ce885ac8c7667
Will Medders <wmedders@gmail.com>
2022-05-16 23:25:26 -0400

    Model Test: relationships and validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,15 @@ require 'rails-helper'
 
 RSpec.describe Customer, type: :model do
   it 'relationships' do
-
+    should { have_many(:invoices) }
+    should { have_many(:transactions).through(:invoices) }
+    should { have_many(:invoice_items).through(:invoices) }
+    should { have_many(:items).through(:invoice_items) }
+    should { have_many(:merchants).through(:items) }
   end
 
   it 'validations' do
-
+    should { validate_presence_of(:first_name) }
+    should { validate_presence_of(:last_name) }
   end
 end
