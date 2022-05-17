dad6c6436bb9d7d0c110eaa586727fd77e6a1c83
Will Medders <wmedders@gmail.com>
2022-05-16 23:42:49 -0400

    Model Test: Merchant relationships and validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   it 'relationships' do
-
+    should { have_many(:items) }
+    should { have_many(:invoice_items).throught(:items) }
+    should { have_many(:invoices).through(:invoice_items) }
+    should { have_many(:transactions.through(:invoices) }
+    should { have_many(:customers.through(:invoices) }
   end
 
   it 'validations' do
-
+    should { validate_presence_of(:name) }
   end
 end
