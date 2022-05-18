fb8c2298ae33775dc89602616abfc7eaa21e0fdc
Will Medders <wmedders@gmail.com>
2022-05-18 13:01:33 -0400

    Feature: items api destroy invoice if this was the only item on invoice

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -20,6 +20,12 @@ class Api::V1::ItemsController < ApplicationController
 
   def destroy
     item = Item.find_by_id(params[:id])
+    invoices = Invoice.find_by_item_id(item.id)
+    invoices.each do |invoice|
+      if invoice.items.count == 1
+        invoice.destroy
+      end
+    end
     item.destroy
   end
 
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :customer
   has_many :transactions
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
 
   validates_presence_of :customer_id
