bd622c20f3bcc342bb15dc8eb7cc08f10f864d70
Will Medders <wmedders@gmail.com>
2022-05-17 21:17:39 -0400

    Feature Test: items api sends one item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the item API' do
-  it 'gets all items' do
+  it 'sends all items' do
     merchant = create(:merchant)
     items = create_list(:item, 10, merchant_id: merchant.id)
 
@@ -36,5 +36,40 @@ RSpec.describe 'the item API' do
       expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
+  end
+
+  it 'sends one item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item.id}"
+    
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+
+
+    expect(response).to be_successful
+    expect(item).to be_a(Hash)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_a(Integer)
   end
 end
