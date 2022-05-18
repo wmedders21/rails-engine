7fd80d714d049aa0811c8ca1f4fddbf62b5754d7
Will Medders <wmedders@gmail.com>
2022-05-18 11:49:41 -0400

    item api sends an updated item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -106,4 +106,41 @@ RSpec.describe 'the item API' do
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item[:attributes][:merchant_id]).to be_a(Integer)
   end
+
+  it 'sends an updated item' do
+    merchant = create(:merchant)
+    seller = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    item_params = { name: 'Chinese Lantern', description: 'Red paper', unit_price: 41.99, merchant_id: seller.id}
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item_params)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+
+    expect(response).to be_successful
+    expect(item).to be_a(Hash)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to eq('Chinese Lantern')
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to eq('Red paper')
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to eq(41.99)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to eq(seller.id)
+  end
 end
