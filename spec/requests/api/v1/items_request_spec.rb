a68799f06cac4dae9fce3a7ca7f14687b7319292
Will Medders <wmedders@gmail.com>
2022-05-17 21:01:47 -0400

    Feature Test: items api gets all items

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'the item API' do
+  it 'gets all items' do
+    merchant = create(:merchant)
+    items = create_list(:item, 10, merchant_id: merchant.id)
+
+    get '/api/v1/items'
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
+
+    expect(response).to be_successful
+    expect(items).to be_a(Array)
+    expect(items.count).to eq(10)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:id]).to be_a(Hash)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+end
