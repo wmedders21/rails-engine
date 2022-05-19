8c2a66a5310e924a1e5630d31261f352775bb69a
Will Medders <wmedders@gmail.com>
2022-05-18 21:23:44 -0400

    postman refactors

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,28 +1,50 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def show
-    if search_params.include?(:name) && search_params[:name]
-      item = Item.name_search(search_params[:name]).first
-      render json: ItemSerializer.new(item)
-    elsif search_params.include?(:min_price) && search_params.include?(:min_price) && search_params[:min_price] && search_params[:max_price]
+    # if search_params.include?(:min_price) && search_params.include?(:min_price) && search_params[:min_price] && search_params[:max_price]
+    #   item = Item.price_range_search(search_params[:min_price].to_i, search_params[:max_price].to_i).first
+    #   render json: ItemSerializer.new(item)
+    # elsif search_params.include?(:min_price) && search_params[:min_price].to_i >= 0 && !search_params.include?(:name)
+    #   item = Item.min_price_search(search_params[:min_price].to_i).first
+    #   render json: ItemSerializer.new(item)
+    # elsif search_params.include?(:max_price) && search_params[:max_price] && !search_params.include?(:name)
+    #   item = Item.max_price_search(search_params[:max_price].to_i).first
+    #   render json: ItemSerializer.new(item)
+    # elsif search_params.include?(:name) && search_params[:name]
+    #     item = Item.name_search(search_params[:name]).first
+    #     render json: ItemSerializer.new(item)
+    # else
+    #   render status: 400
+    # end
+
+    if search_params.include?(:min_price) && search_params.include?(:max_price) && search_params[:min_price].to_i >= 0 && search_params[:max_price].to_i >= 0 && !search_params.include?(:name)
       item = Item.price_range_search(search_params[:min_price].to_i, search_params[:max_price].to_i).first
       render json: ItemSerializer.new(item)
-    elsif search_params.include?(:min_price) && search_params[:min_price]
+    elsif search_params.include?(:min_price) && search_params[:min_price].to_i >= 0 && !search_params.include?(:name) && !search_params.include?(:max_price)
       item = Item.min_price_search(search_params[:min_price].to_i).first
       render json: ItemSerializer.new(item)
-    elsif search_params.include?(:max_price) && search_params[:max_price]
+    elsif search_params.include?(:max_price) && search_params[:max_price].to_i >= 0 && !search_params.include?(:name) && !search_params.include?(:min_price)
       item = Item.max_price_search(search_params[:max_price].to_i).first
       render json: ItemSerializer.new(item)
+    elsif search_params.include?(:name) && search_params[:name]
+        item = Item.name_search(search_params[:name]).first
+        render json: ItemSerializer.new(item)
     else
-      render status: 404
+      render json: { error: "bad request" }, status: 400
     end
   end
-
-
 
   private
 
   def search_params
     params.permit(:name, :min_price, :max_price)
   end
+
+  # def name_search_params
+  #   params.permit(:name)
+  # end
+  #
+  # def price_search_params
+  #   params.permit(:min_price, :max_price)
+  # end
 end
