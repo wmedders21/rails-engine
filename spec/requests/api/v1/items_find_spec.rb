a8bf3d26b3bb434c516fe0dbf272fa52762ba9a9
Will Medders <wmedders@gmail.com>
2022-05-18 20:11:34 -0400

    Feature Test: the item search api sends a single object for a price_range search

--- spec/requests/api/v1/items_find_spec.rb
+++ spec/requests/api/v1/items_find_spec.rb
@@ -103,5 +103,34 @@ RSpec.describe 'the item search api' do
 
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item[:attributes][:merchant_id]).to be_a(Integer)
+
+    get '/api/v1/items/find?min_price=40&max_price=120'
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+
+    expect(response).to be_successful
+    expect(item).to be_a(Hash)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to eq('Blueberry')
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_a(Integer)
   end
 end
